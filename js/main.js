d2b1818df2617dde097406fe8efc7ad14bd8419f
Maria Pomogaeva <maria.pomogaeva1@gmail.com>
2023-03-14 16:38:28 +0700

    Исправление после проверки

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-import './generate-post-data.js';
-import './user-thumbnails.js';
+import {createPostData} from './mocks/generate-post-data.js';
+import {renderThumbnails} './user-thumbnails.js';
+
+const SIMILAR_POST_COUNT = 25;
+const similarPosts = createPostData(SIMILAR_POST_COUNT);
+renderThumbnails(similarPosts);
--- js/user-thumbnails.js
+++ js/user-thumbnails.js
@@ -1,23 +1,20 @@
-import {createPostData} from './generate-post-data.js';
-
-const pictureAll = document.querySelector('.picture');
-const pictureImg = pictureAll.querySelector('#picture').content.querySelector('.picture__img');
-const pictureComments = pictureAll.querySelector('#picture').content.querySelector('.picture__comments');
-const pictureLikes = pictureAll.querySelector('#picture').content.querySelector('.picture__likes');
-
-const similarPost = createPostData();
+const picturesWrapper = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content;
 
 const similarPostFragment = document.createDocumentFragment();
 
-similarPost.forEach( function(url, comments, likes) {
-  const pictureElementImg = pictureImg.cloneNode(true);
-  const pictureElementComments = pictureComments.cloneNode(true);
-  const pictureElementLikes = pictureLikes.cloneNode(true);
+/**
+ * @parm {element} element — элемент массива
+ */
+similarPosts.forEach((url, comments, likes) => {
+  const newPictureTemplate = pictureTemplate.cloneNode(true);
 
-  pictureElementImg.querySelector('.picture__img').textContent = url;
-  pictureElementComments.querySelector('.picture__comments').textContent = comments;
-  pictureElementLikes.querySelector('.picture__likes').textContent = likes;
-
+  newPictureTemplate.querySelector('.picture__img').textContent = url;
+  newPictureTemplate.querySelector('.picture__comments').textContent = comments.length;
+  newPictureTemplate.querySelector('.picture__likes').textContent = likes;
+  similarPostFragment.appendChild(newPictureTemplate);
 });
 
-similarPostFragment.appendChild(similarPostFragment);
+picturesWrapper.appendChild(similarPostFragment);
+
+export {similarPosts};
