82f6edbc80078829bd60c3a5930863a3788c0f00
Maria Pomogaeva <maria.pomogaeva1@gmail.com>
2023-03-24 11:42:07 +0700

    Загрузка последующих комментариев

--- js/open-big-picture.js
+++ js/open-big-picture.js
@@ -3,11 +3,11 @@ import {isEscapeKey} from './util.js';
 const MAX_COMMENTS_NUMBER = 5;
 
 const closeBigPicture = document.querySelector('.big-picture__cancel');
-// const commentLoad = document.querySelector('.comments-loader');
+
 const commentTemplateElement = document.querySelector('.social__comment');
 /**
- * Отрисовка комментария для потса
- * @param {object[]} массив всех комментариев для потса
+ * Отрисовка комментария для поста
+ * @param {object[]} массив всех комментариев для поста
  * @returns {object} кусок разметки
  */
 const renderComment = ({avatar, message, nameComment}) => {
@@ -21,6 +21,7 @@ const renderComment = ({avatar, message, nameComment}) => {
 
 const commentListElement = document.querySelector('.social__comments');
 const commentCountElement = document.querySelector('.social__comment-count');
+const commentLoadButton = document.querySelector('.comments-loader');
 /**
  * Отрисовывает первые пять комментариев
  * @param {object[]} comments все комментарии поста
@@ -29,17 +30,29 @@ const renderComments = (allComments) => {
   const firstComments = allComments.slice(0, MAX_COMMENTS_NUMBER);
   const commentFragment = document.createDocumentFragment;
   firstComments.forEach((comment) => {
-    const commentElement = renderComment(comment);
+    const commentElement = renderComments(comment);
     commentFragment.appendChild(commentElement);
   });
   commentListElement.appendChild(commentFragment);
 
-  commentCountElement.firstChild.textContent = `${firstComments.length } из ${allComments}`;
+  commentCountElement.firstChild.textContent = `${firstComments.length} из ${allComments.length}`;
 
   // Проверка на все ли комментарии отрисованы -> Показываем\убираем кнопку
-  // if (firstComments === allComments) {
+  if (firstComments === allComments) {
+    commentLoadButton.classList.remove('hidden');
+  } else if (firstComments < allComments) {
+    commentLoadButton.addEventListener('click', () => {
+      const followComments = allComments.slice(0, MAX_COMMENTS_NUMBER);
+      const commentFragmentFollow = document.createDocumentFragment;
+      followComments.forEach((comment) => {
+        const commentElementFollow = renderComments(comment);
+        commentFragmentFollow.appendChild(commentElementFollow);
+      });
+      commentListElement.appendChild(commentFragmentFollow);
 
-  // };
+      commentCountElement.firstChild.textContent = `${followComments.length} из ${allComments.length}`;
+    });
+  }
   // Если усть кнопка, вешаем событие на отрисовку остальных комменатриев (внутри этой функции проверка показывать или убирать кнопк дальше)
 };
 
